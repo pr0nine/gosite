bf46d1fb633f4e2f9570aeb388810507afe20e02
Pronay <prona@localhost.localdomain>
2024-10-31 12:26:35 +0530

    root dir remapped

--- main.go
+++ main.go
@@ -8,17 +8,25 @@ import (
 )
 
 func main()  {
+
+  http.Handle("/styles/", http.StripPrefix("/styles/", http.FileServer(http.Dir("styles/"))))
   http.HandleFunc("/", home)
+  // server starts on port 80
+  err := http.ListenAndServe(":8080", nil)
+  if err != nil {
+    log.Fatal(err)
+  }
+  fmt.Println("ahoy your server is running ")
 }
 
 //home function handle requests to home package
-func home(w http.ResponseWritter, r *http.Request)  {
+func home(w http.ResponseWriter, r *http.Request)  {
   renderTemplate(w, "home.html")
 }
 
-func renderTemplate(w http.ResponseWritter, tmpl string)  {
+func renderTemplate(w http.ResponseWriter, tmpl string)  {
   // parsing the specified template file as input 
-  t,err := template.ParseFiles("template/" + tmpl)
+  t, err := template.ParseFiles("template/" + tmpl)
   if err != nil {
     http.Error(w, err.Error(), http.StatusInternalServerError)
   }
